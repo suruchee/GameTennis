3aac53a9cafcdaf3803c263ac02833942369be5e
suruchee <suruchee11@gmail.com>
2018-12-15 20:15:07 +0545

    game project

---
+++ src/projectGame/Keyboard.java
@@ -0,0 +1,41 @@
+package projectGame;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Keyboard extends JPanel {
+	
+	public Keyboard() {
+		KeyListener listener = new MyKeyListener();
+		addKeyListener(listener);
+		setFocusable(true);
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame(" Tennis");
+		Keyboard keyboard = new Keyboard();
+		frame.add(keyboard);
+		frame.setSize(200, 200);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public class MyKeyListener implements KeyListener {
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("keyReleased="+KeyEvent.getKeyText(e.getKeyCode()));
+		}
+	}
+}
